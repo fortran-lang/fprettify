41b8cc709e5cd080415ac96708003c048da850b2
Tiziano Müller <tm@dev-zero.ch>
2016-08-31 12:52:33 +0200

    setup.py: fix version number spec

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='fprettify',
-      version='v0.1',
+      version='0.1',
       description='auto-formatter for modern fortran source code',
       author='Patrick Seewald, Ole Schuett, Mohamed Fawzi',
       license='GPL',
