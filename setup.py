90b0c12672b37b2fee53e876c912dba0e4a73771
Patrick Seewald <patrick.seewald@gmail.com>
2016-12-25 11:11:34 +0100

    fix version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='fprettify',
-      version='0.1',
+      version='0.3',
       description='auto-formatter for modern fortran source code',
       author='Patrick Seewald, Ole Schuett, Tiziano Mueller, Mohamed Fawzi',
       license='GPLv3',
