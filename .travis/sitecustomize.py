679867d3ea09a8f9986667620ec2c5641f1fa58d
Patrick Seewald <patrick.seewald@gmail.com>
2016-12-31 22:33:20 +0100

    last try including subprocess into coverage

--- .travis/sitecustomize.py
+++ .travis/sitecustomize.py
@@ -1,2 +1,2 @@
-import coverage
-coverage.process_startup()
+import site, os, inspect
+site.addsitedir(mypath = os.path.join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))), known_paths=None)
