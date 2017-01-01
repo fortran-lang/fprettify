bb58a80065a4b117f415941ac3ea926790bb7f21
Patrick Seewald <patrick.seewald@gmail.com>
2017-01-01 12:00:18 +0100

    forgot to add run script

---
+++ fprettify.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""wrapper script to run fprettify"""
+
+from fprettify import run # pragma: no cover
+
+if __name__ == '__main__': # pragma: no cover
+    run()
