18497b48a2b8fbb5c335b391d40ce16f5d9949c1
Patrick Seewald <patrick.seewald@gmail.com>
2018-08-22 15:28:46 +0200

    preparing 0.3.3a pre-release

--- fprettify/__init__.py
+++ fprettify/__init__.py
@@ -1318,7 +1318,7 @@ def run(argv=sys.argv):  # pragma: no cover
     parser.add_argument('-f', '--fortran', type=str, action='append', default=[],
                         help="Overrides default fortran extensions recognized by --recursive. Repeat this option to specify more than one extension.")
     parser.add_argument('--version', action='version',
-                        version='%(prog)s 0.3.2')
+                        version='%(prog)s 0.3.3a')
 
     args = parser.parse_args(argv[1:])
 
