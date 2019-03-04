8f78aa8d3bcc2948f03bc3158ca8be7afe72f8f7
Patrick Seewald <patrick.seewald@gmail.com>
2019-03-04 17:33:59 +0100

    fix travis cron job

--- .travis/prep_cp2k.sh
+++ .travis/prep_cp2k.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 wget https://github.com/pseewald/cp2k/archive/fprettify-test.tar.gz
-tar -xf fprettify-test.tar.gz -C fortran_tests/before --strip-components=1 cp2k-fprettify-test/cp2k/src
+mkdir fortran_tests/before/cp2k
+tar -xf fprettify-test.tar.gz -C fortran_tests/before/cp2k --strip-components=1 cp2k-fprettify-test/src
 rm fprettify-test.tar.gz
